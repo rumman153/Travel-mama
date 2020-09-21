d9ec854d9b0b7f4bfdd2238f92736b80d7906ee8
Rumman Ahmed Prodhan <rumman153@gmail.com>
2020-09-21 13:01:21 +0600

    Post Foreignkey
    Post er foreignkey add korsi

--- PostManagement/models.py
+++ PostManagement/models.py
@@ -1,5 +1,5 @@
-
 from django.db import models
+from UserManagement.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -8,4 +8,6 @@ class Post(models.Model):
     Post_catagory = models.CharField(max_length=50)
     Post_tags = models.CharField(max_length=200)
     Post_description = models.TextField(max_length=100000)
+
+    User = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
