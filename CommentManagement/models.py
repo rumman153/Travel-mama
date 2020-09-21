9730c6e172a3ec2a0eae6d55e53967fcb82f5209
rafsan ☀️ <18101009@uap-bd.edu>
2020-09-21 13:54:09 +0600

    Foreign Key
    Adding in CommentManagement

--- CommentManagement/models.py
+++ CommentManagement/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from UserManagement.models import User
+from PostManagement.models import Post
 
 # Create your models here.
 class Comment(models.Model):
-    Comment_description = models.CharField(max_length=5000)
+    Comment_description = models.TextField(max_length=5000)
+
+    User = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
