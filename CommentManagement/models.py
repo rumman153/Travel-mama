7fb4bd9dbf0f8ad093d7b62c899022e1f1b8ec94
Rumman Ahmed Prodhan <rumman153@gmail.com>
2020-10-01 12:14:01 +0600

    Corrections

--- CommentManagement/models.py
+++ CommentManagement/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from UserManagement.models import User
+from ProfileManagement.models import Profile
 from PostManagement.models import Post
 
 # Create your models here.
 class Comment(models.Model):
     Comment_description = models.TextField(max_length=5000)
 
-    User = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     Post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
--- PostManagement/models.py
+++ PostManagement/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ProfileManagement.models import Profile
 
 
 # Create your models here.
@@ -18,6 +19,7 @@ class Post(models.Model):
     #Post_image3 = models.ImageField(null='true',blank='true')
     #Post_description3 = models.TextField(max_length=100000, null='true',blank='true')
 
+    Profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.Post_title
